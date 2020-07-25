d91aa2604ab39b9c2666529e3bcd7de5de0212a2
Lander Van laer <lander.vanlaer1@gmail.com>
2020-07-25 19:53:24 +0200

    Create Number.java

---
+++ src/me/landervanlaer/math/Number.java
@@ -0,0 +1,29 @@
+package me.landervanlaer.math;
+
+public class Number {
+    /**
+     * Get a random value between 2 numbers.
+     * <p>This method uses {@link Math#random()}</p>
+     *
+     * @param min The minimum value (included)
+     * @param max The maximum value (excluded)
+     * @return A random number
+     */
+    public static int getRandom(int min, int max) {
+        return (int) (Math.random() * (max - min) + min);
+    }
+
+    /**
+     * Re-maps a number from a range to another
+     *
+     * @param value The number that needs to be converted
+     * @param min1  The minimum value of the first range
+     * @param max1  The maximum value of the first range
+     * @param min2  The minimum value of the second range
+     * @param max2  The maximum value of the second range
+     * @return The remapped number
+     */
+    public static double map(double value, double min1, double max1, double min2, double max2) {
+        return (value - min1) / (max1 - min1) * (max2 - min2) + min2;
+    }
+}
