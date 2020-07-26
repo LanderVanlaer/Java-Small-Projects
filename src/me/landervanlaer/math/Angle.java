c0e923e9386a793b4a68d47d56222a319a28729c
Lander Van laer <lander.vanlaer1@gmail.com>
2020-07-26 18:03:01 +0200

    Adding simplifying angles

--- src/me/landervanlaer/math/Angle.java
+++ src/me/landervanlaer/math/Angle.java
@@ -4,15 +4,23 @@ public class Angle {
     private double rad;
     /**
      * If the {@link Angle} has to be simplified
+     * <p>
      * <code>
      * //{@code true}
      * 370° == 10°
-     * <p>
      * //{@code false}
      * 370° != 10°
      * </code>
      */
-    private boolean simplify;
+    private boolean simplify = true;
+
+    public Angle() {
+        this(0);
+    }
+
+    public Angle(double rad) {
+        this.setRadians(rad);
+    }
 
     static public double degreesToRadians(double degrees) {
         return degrees / (180 / Math.PI);
@@ -40,8 +48,19 @@ public class Angle {
         return degrees;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || this.getClass() != o.getClass()) return false;
+        Angle angle = (Angle) o;
+        return angle.getRadians() == this.getRadians();
+    }
+
     public double getRadians() {
-        return rad;
+        if(this.isSimplify())
+            return Angle.simplifyRadians(rad);
+        else
+            return rad;
     }
 
     public void setRadians(double rad) {
@@ -49,10 +68,21 @@ public class Angle {
     }
 
     public double getDegrees() {
-        return Angle.radiansToDegrees(this.getRadians());
+        if(this.isSimplify())
+            return Angle.simplifyDegrees(Angle.radiansToDegrees(this.getRadians()));
+        else
+            return Angle.radiansToDegrees(this.getRadians());
     }
 
     public void setDegrees(double degrees) {
         this.setRadians(Angle.degreesToRadians(degrees));
+    }
+
+    public boolean isSimplify() {
+        return simplify;
+    }
+
+    public void setSimplify(boolean simplify) {
+        this.simplify = simplify;
     }
 }
