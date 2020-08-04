4f84188187f35d2052b96d2767b9db8a8dc13984
Lander Van laer <lander.vanlaer1@gmail.com>
2020-08-04 17:12:49 +0200

    copy Tic Tac Toe from other project

---
+++ src/me/landervanlaer/games/TicTacToe/Controller.java
@@ -0,0 +1,69 @@
+package me.landervanlaer.games.TicTacToe;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+    public Button button8;
+    public Button button9;
+    private Button[][] buttons;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        button1.setText("nen");
+        buttons = new Button[][]{
+                {button1, button4, button7},
+                {button2, button5, button8},
+                {button3, button6, button9}
+        };
+
+        for(int i = 0; i < buttons.length; i++)
+            for(int j = 0; j < buttons[0].length; j++) {
+                final Button button = buttons[i][j];
+//                button.setText(String.valueOf((i + 1) + j * 3));
+                button.setText("");
+
+                button.setOnMouseClicked((MouseEvent e) -> {
+                    pressButton(button);
+                });
+            }
+    }
+
+    /**
+     * A method that handles a {{@link MouseEvent}}
+     *
+     * @param button The button that has been pressed
+     */
+    public void pressButton(Button button) {
+        final int[] pos = this.getIndexOfButton(button);
+        if(pos == null) return;
+        Game.clickButton(button, pos);
+        Game.validate(buttons);
+    }
+
+    /**
+     * Get the x and y position of a button
+     *
+     * @param btn The button whose position you need
+     * @return The position
+     */
+    public int[] getIndexOfButton(Button btn) {
+        for(int i = 0; i < buttons.length; i++)
+            for(int j = 0; j < buttons[0].length; j++) {
+                if(buttons[i][j] == btn) return new int[]{i, j};
+            }
+        return null;
+    }
+}
---
+++ src/me/landervanlaer/games/TicTacToe/Game.java
@@ -0,0 +1,75 @@
+package me.landervanlaer.games.TicTacToe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+    public static char[] PLAYER_CHAR = new char[]{'X', 'O'};
+    private static boolean playerTurn1 = true;
+    private static String text = "";
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Tic Tac Toe");
+        Parent root = FXMLLoader.load(this.getClass().getResource("me\\landervanlaer\\games\\TicTacToe\\GameLayout.fxml"));
+        Scene scene = new Scene(root, 800, 800);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void clickButton(Button btn, int[] pos) {
+        if(btn.getText() == "")
+            btn.setText(String.valueOf(PLAYER_CHAR[playerTurn1 ? 0 : 1]));
+        playerTurn1 = !playerTurn1;
+    }
+
+    /**
+     * see if anyone won
+     *
+     * @param buttons the button grid to validate
+     */
+    public static void validate(Button[][] buttons) {
+
+        for(int i = 0; i < 3; i++) {
+            if(!buttons[i][0].getText().equals("") && buttons[i][0].getText().equals(buttons[i][1].getText()) && buttons[i][1].getText().equals(buttons[i][2].getText())) {
+                playerWon(buttons[i][0].getText().charAt(0));
+                return;
+            }
+            if(!buttons[0][i].getText().equals("") && buttons[0][i].getText().equals(buttons[1][i].getText()) && buttons[1][i].getText().equals(buttons[2][i].getText())) {
+                playerWon(buttons[0][i].getText().charAt(0));
+                return;
+            }
+        }
+
+        if(!buttons[1][1].getText().equals("") &&
+                ((buttons[0][0].getText().equals(buttons[1][1].getText()) && buttons[1][1].getText().equals(buttons[2][2].getText())) ||
+                        (buttons[0][2].getText().equals(buttons[1][1].getText()) && buttons[1][1].getText().equals(buttons[2][0].getText())))) {
+
+            playerWon(buttons[1][1].getText().charAt(0));
+            return;
+        }
+    }
+
+    private static void playerWon(char player) {
+        Game.setText("Player " + (new String(PLAYER_CHAR).indexOf(player) + 1) + " won!");
+    }
+
+    public static String getText() {
+        return text;
+    }
+
+    public static void setText(String text) {
+        Game.text = text;
+        System.out.println(Game.getText());
+    }
+}
