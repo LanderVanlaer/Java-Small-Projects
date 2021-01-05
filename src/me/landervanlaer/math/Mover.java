bddc3310a9d6a26aba02ab875c40c2bc1db8e62d
Lander Van laer <lander.vanlaer1@gmail.com>
2021-01-05 18:21:15 +0100

    Create Mover object

---
+++ src/me/landervanlaer/math/Mover.java
@@ -0,0 +1,165 @@
+package me.landervanlaer.math;
+
+import me.landervanlaer.objects.Drawable;
+
+/**
+ * A {@link Drawable} {@link Object} that has a position, velocity and acceleration.
+ * It represents an Object that can move
+ *
+ * @author Lander Van laer
+ * @version 1.0 2021/01/5
+ */
+abstract public class Mover implements Drawable {
+    /**
+     * <strong>Position</strong>
+     * <p>
+     * The current position of the {@link Mover} Object
+     *
+     * @see #getPos()
+     * @see #setPos(Coordinate)
+     */
+    private Coordinate pos;
+    /**
+     * <strong>Velocity</strong>
+     * <p>
+     * The current velocity of the {@link Mover} Object.
+     * <p>
+     * The velocity {@link Vector} represents the speed at which the {@link #pos} of the object changes.
+     * The magnitude of a velocity vector indicates the speed of a object,
+     * while the Vectors {@link Angle} indicates the direction.
+     *
+     * @see #getVel()
+     * @see #setVel(Vector)
+     * @see Vector#getAngle()
+     * @see Vector#getMag()
+     */
+    private Vector vel;
+    /**
+     * <strong>Acceleration</strong>
+     * <p>
+     * The Acceleration of an {@link Object} is the change
+     * in its {@link #vel} over a certain period of time,
+     * or the Velocity at which the speed increases.
+     *
+     * @see #getAcc()
+     * @see #setAcc(Vector)
+     */
+    private Vector acc;
+    /**
+     * Defines the mass of the {@link Object} itself.
+     * <p>
+     * When applying a {@link Vector} to the {@link Mover},
+     * the Vector will be divided by the mass and then added.
+     *
+     * @see #getMass()
+     * @see #setMass(double)
+     * @see #applyForce(Vector)
+     */
+    private double mass;
+
+    /**
+     * Defines a new {@link Mover} Object, with a given position, mass and velocity of {@code zero}.
+     *
+     * @param pos  {@link #pos}, The current position of the Object
+     * @param mass {@link #mass}, The mass of the Object
+     * @see #Mover(Coordinate, Vector, double)
+     */
+    public Mover(Coordinate pos, double mass) {
+        this(pos, new Vector(), mass);
+    }
+
+    /**
+     * Defines a new {@link Mover} Object, with a given position, mass and velocity.
+     *
+     * @param pos  {@link #pos}, The current position of the Object
+     * @param vel  {@link #vel}, The current velocity that the Object has
+     * @param mass {@link #mass}, The mass of the Object
+     * @see #Mover(Coordinate, double)
+     */
+    public Mover(Coordinate pos, Vector vel, double mass) {
+        this.pos = pos;
+        this.vel = vel;
+        this.mass = mass;
+        this.acc = new Vector();
+    }
+
+    /**
+     * The {@link #acc} will be added to the {@link #vel}, while the vel will be added to the current {@link #pos}.
+     * <pre>
+     *     add acc to vel
+     *     add vel to current pos
+     *     set acc = empty {@link Vector};
+     * </pre>
+     *
+     * @see Coordinate#add(Vector)
+     */
+    public void update() {
+        getVel().add(getAcc());
+        getPos().add(getVel());
+        getAcc().zero();
+    }
+
+    /**
+     * Adds the force to the {@link #acc} by first dividing it by the {@link #mass}
+     *
+     * @param force The force you want to add to the {@link #acc}
+     * @see #mass
+     */
+    public void applyForce(Vector force) {
+        getAcc().add(Vector.div(force, getMass()));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(!(o instanceof Mover)) return false;
+
+        Mover mover = (Mover) o;
+
+        if(getPos() != null ? !getPos().equals(mover.getPos()) : mover.getPos() != null) return false;
+        if(getVel() != null ? !getVel().equals(mover.getVel()) : mover.getVel() != null) return false;
+        return getAcc() != null ? getAcc().equals(mover.getAcc()) : mover.getAcc() == null;
+    }
+
+    @Override
+    public String toString() {
+        return "Mover{" +
+                "pos=" + pos +
+                ", vel=" + vel +
+                ", acc=" + acc +
+                '}';
+    }
+
+    // GETTERS AND SETTERS
+    public Coordinate getPos() {
+        return pos;
+    }
+
+    public void setPos(Coordinate pos) {
+        this.pos = pos;
+    }
+
+    public Vector getVel() {
+        return vel;
+    }
+
+    public void setVel(Vector vel) {
+        this.vel = vel;
+    }
+
+    public Vector getAcc() {
+        return acc;
+    }
+
+    public void setAcc(Vector acc) {
+        this.acc = acc;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+}
